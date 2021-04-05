5916b56bf224796b83aa18730604a6e3288f0c73
nikita-t5 <nickita.tashirev@yandex.ru>
2021-04-05 21:08:14 +0300

    using Prepared statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         DBWorker worker = new DBWorker(); //создание connection
+        String insert = "INSERT INTO users (name, age, email) VALUES(?,?,?);"; //подготовленное выражение - шаблон запроса. можно исп синтаксис (:1, :2,...)
+
+        try (PreparedStatement preparedStatement = worker.getConnection().prepareStatement(insert)) {
+            preparedStatement.setString(1, "Lee"); //выставление 1-го параметра для запроса
+            preparedStatement.setInt(2, 78); //выставлениу 2-го параметра для запроса
+            preparedStatement.setString(3, "emaillee.com");
+            preparedStatement.execute(); //выполнить запрос
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         try (Statement statement = worker.getConnection().createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users"); //resultSet - полная модель всей таблицы
             while (resultSet.next()) {  //Перебор строк с данными. Изначально курсор на первой позиции. next() - сместсить курсор на одну строку
